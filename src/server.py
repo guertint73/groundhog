e803c0fb22428d5d6f6b39b07395781e66be1a87
Timothy Guertin <guertint@msu.edu>
2020-05-13 22:26:31 -0500

    404 not found

--- src/server.py
+++ src/server.py
@@ -9,9 +9,16 @@ app = Flask(__name__)
 @app.route('/<state>')
 def get_covid_data_by_state(state):
     states_json = CSVService.get_covid_states_csv(state=state)
-    response = Response(response=json.dumps(states_json),
-                        status=200,
-                        mimetype='application/json')
+
+    if states_json is None:
+        response = Response(response=json.dumps({"msg": "Resource Not Found"}),
+                            status=404,
+                            mimetype='application/json')
+
+    else:
+        response = Response(response=json.dumps(states_json),
+                            status=200,
+                            mimetype='application/json')
     return response
 
 
--- src/services/csv_service.py
+++ src/services/csv_service.py
@@ -1,16 +1,19 @@
-# import requests
-# import json
 import util.constants as Constants
 import pandas as pd
 
 
-def get_covid_states_csv(state='Minnesota'):
+def get_covid_states_csv(state=None):
     '''
     Get COVID-19 cases by state
     '''
+
     state_df = pd.read_csv(f'src/resources/{Constants.STATES_FILENAME}')
 
     state_dict = state_df[(state_df.state == state)].to_dict("records")
+
+    if state_dict == []:
+        return None
+
     return state_dict
 
 
