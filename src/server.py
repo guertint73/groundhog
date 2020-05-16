39715346dfc3e8fa25cceebbb08f5b50970345eb
Timothy Guertin <guertint@msu.edu>
2020-05-15 19:22:59 -0500

    error class, error messages, capitalize states names

--- src/server.py
+++ src/server.py
@@ -1,17 +1,22 @@
 from flask import Flask, Response
 import json
 import services.csv_service as CSVService
+from util.errors import Error
 
 '''Flask Setup'''
 app = Flask(__name__)
 
 
-@app.route('/<state>')
+@app.route('/groundhog/v1/state/<state>')
 def get_covid_data_by_state(state):
     states_json = CSVService.get_covid_states_csv(state=state)
 
-    if states_json is None:
-        response = Response(response=json.dumps({"msg": "Resource Not Found"}),
+    if states_json is Error.NOT_FOUND:
+        err_msg = {
+            'error': 'Resource not found',
+            'resource': state
+        }
+        response = Response(response=json.dumps(err_msg),
                             status=404,
                             mimetype='application/json')
 
